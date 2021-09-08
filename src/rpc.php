d7f53d0def4394db712f7f27bcb9e5c25ff6720d
whereof <wangzhiqiang1224@gmail.com>
2021-09-09 02:14:00 +0800

    中间价 && 过滤器

--- src/rpc.php
+++ src/rpc.php
@@ -1,10 +1,20 @@
 <?php
 
 
-use whereof\hprose\Services\UserServer;
-
 \whereof\hprose\Facades\HproseRoute::add(function () {
     return 'service hello';
 }, 'hello');
 
-\whereof\hprose\Facades\HproseRoute::add(UserServer::class);
+\whereof\hprose\Facades\HproseRoute::add(function () {
+    return 'LogFilter';
+}, 'myfilter')->addFilter(new \whereof\hprose\Filter\LogFilter());
+
+\whereof\hprose\Facades\HproseRoute::add(function ($a, $b) {
+    return 'InvokeHandler 参数：'.$a.'.'.$b;
+}, 'myhandler')->addInvokeHandler(function ($name, array &$args, stdClass $context, Closure $next) {
+    \whereof\hprose\Support\LaravelHelper::log('调用的远程函数/方法名:' . $name, 'debug', $args);
+    $result = $next($name, $args, $context);
+    return $result;
+});
+
+\whereof\hprose\Facades\HproseRoute::add(\whereof\hprose\Services\UserServer::class);
