2266d362230cccdbf65d12271937c6faf6281604
wangzhiqiang <40115555+whereof@users.noreply.github.com>
2021-09-09 22:16:29 +0800

    Update rpc.php

--- src/rpc.php
+++ src/rpc.php
@@ -1,20 +1,14 @@
 <?php
-
-
-\whereof\hprose\Facades\HproseRoute::add(function () {
+use whereof\hprose\Facades\HproseRoute;
+// 注册callback
+HproseRoute::add(function () {
     return 'service hello';
 }, 'hello');
-
-\whereof\hprose\Facades\HproseRoute::add(function () {
-    return 'LogFilter';
-}, 'myfilter')->addFilter(new \whereof\hprose\Filter\LogFilter());
-
-\whereof\hprose\Facades\HproseRoute::add(function ($a, $b) {
-    return 'InvokeHandler 参数：'.$a.'.'.$b;
-}, 'myhandler')->addInvokeHandler(function ($name, array &$args, stdClass $context, Closure $next) {
+// 注册class
+HproseRoute::add(\whereof\hprose\Services\UserServer::class);
+//注册中间价
+HproseRoute::addInvokeHandler(function ($name, array &$args, stdClass $context, Closure $next) {
     \whereof\hprose\Support\LaravelHelper::log('调用的远程函数/方法名:' . $name, 'debug', $args);
     $result = $next($name, $args, $context);
     return $result;
 });
-
-\whereof\hprose\Facades\HproseRoute::add(\whereof\hprose\Services\UserServer::class);
